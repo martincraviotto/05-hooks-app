8646e6f3e81d38c41da1fcbf6691ffc35f90a9b2
Martin Craviotto <martincraviotto@gmail.com>
2023-01-12 11:24:26 -0300

    Final Clase118

--- src/01-useState/CounterWithCustomHook.jsx
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -3,7 +3,7 @@ import { useCounter } from '../hooks/useCounter';
 
 export const CounterWithCustomHook = () => {
 
-    const {counter} = useCounter();
+    const {counter,increment,decrement,reset} = useCounter();
 
 
     return (
@@ -11,9 +11,20 @@ export const CounterWithCustomHook = () => {
             <div>Counter con Hook : {counter}</div>
             <hr/>
 
-            <button className='btn btn-primary'>+1</button>
-            <button className='btn btn-primary'>Reset</button>
-            <button className='btn btn-primary'>-1</button>
+            <button 
+                className='btn btn-primary'
+                onClick={()=>increment(2)}
+            > +1 </button>
+
+            <button 
+                className='btn btn-primary'
+                onClick={reset}
+            >Reset</button>
+
+            <button 
+                className='btn btn-primary'
+                onClick={()=>decrement(2)}
+            >-1</button>
         </>
     )
 }
--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,12 +1,28 @@
 import { useState } from "react"
 
 
-export const useCounter = (initialVlue=10)=>{
+export const useCounter = (initialValue=10)=>{
 
-    const [counter, setCounter] = useState(initialVlue);
+    const [counter, setCounter] = useState(initialValue);
+
+    const increment = (value=1)=>{        
+        setCounter(counter + value);
+    }
+
+    const decrement = (value=1)=>{
+        //if(counter === 0) return;
+        setCounter(counter - value);
+    }
+
+    const reset = ()=>{
+        setCounter(initialValue);
+    }
 
     return{
         counter,
+        increment,
+        decrement,
+        reset
     }
 
 
