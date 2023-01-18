02b7f56d05a60e9375a31b689eeb68513b8d05a5
Martin <martincraviotto@gmail.com>
2023-01-18 20:12:26 -0300

    Final Clase132

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import ReactDOM from 'react-dom/client';
 //import { MultiplecustomHooks } from './03-examples/MultiplecustomHooks';
 //import { FocusScreen } from './04-useRef/FocusScreen';
 //import { Layout } from './05-useLayoutEffect/Layout';
-import { Memorize } from './06-memos/Memorize';
+//import { Memorize } from './06-memos/Memorize';
+import { MemoHook } from './06-memos/MemoHook';
 import './index.css';
 
 
@@ -15,7 +16,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
-    <Memorize />
+    <MemoHook />
   //</React.StrictMode>
 );
 
