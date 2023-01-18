e0a488c7f52e80a89e448d44c3dc43d3612db1a2
Martin <martincraviotto@gmail.com>
2023-01-18 20:51:51 -0300

    Clase133

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import ReactDOM from 'react-dom/client';
 //import { FocusScreen } from './04-useRef/FocusScreen';
 //import { Layout } from './05-useLayoutEffect/Layout';
 //import { Memorize } from './06-memos/Memorize';
-import { MemoHook } from './06-memos/MemoHook';
+//import { MemoHook } from './06-memos/MemoHook';
+import { CallbackHook } from './06-memos/CallbackHook';
 import './index.css';
 
 
@@ -16,7 +17,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
-    <MemoHook />
+    <CallbackHook />
   //</React.StrictMode>
 );
 
