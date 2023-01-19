60f0f5b328be7e1586c32279f24ac55548e812bd
Martin <martincraviotto@gmail.com>
2023-01-19 13:18:22 -0300

    Final Clase144-145

---
+++ src/08-useReducer/TodoAdd.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useForm } from '../hooks/useForm'
+
+export const TodoAdd = ( {onNewTodo} ) => {
+
+    const {description, onInputChange, onResetForm}=useForm({description:''});
+
+    const onFormSubmit = (event)=>{
+        event.preventDefault();
+
+        if(description.length <= 1) return;
+
+        const newTodo ={
+            id: new Date().getTime(),
+            done:false,
+            description, description,
+        }
+        
+        onNewTodo && onNewTodo(newTodo);
+        onResetForm();
+
+    }
+
+  return (
+    <form onSubmit={onFormSubmit}>
+        <input 
+            type="text" 
+            placeholder='Que hay que hacer?'
+            className='form-control'
+            name="description"
+            value={description}
+            onChange={onInputChange}
+        />
+        <button 
+            type="submit"
+            className='btn btn-outline-primary'
+        >
+            Agregar
+        </button>
+    </form>
+  )
+}
---
+++ src/08-useReducer/TodoItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const TodoItem = ({todo}) => {
+   
+  return (
+    <li  className='list-group-item d-flex justify-content-between'>
+        <span className='align-self-item'> { todo.description } </span>
+        <button className='btn btn-danger'>Borrar</button>
+    </li> 
+  )
+}
