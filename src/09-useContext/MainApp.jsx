958a5ff1bf4c068706ef5218b35954e2848c894e
Martin <martincraviotto@gmail.com>
2023-01-20 21:07:20 -0300

    Final Clase157

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { AboutPage } from './AboutPage'
+import { HomePage } from './HomePage'
+import { LoginPage } from './LoginPage'
 
 export const MainApp = () => {
   return (
     <>
         <h1>Main App</h1>
         <hr />
+
+        <Routes>
+          <Route path="/" element={<HomePage /> }/>
+          <Route path="login" element={<LoginPage /> }/>
+          <Route path="about" element={<AboutPage /> }/>          
+
+          {/* <Route path="/*" element={<LoginPage /> }/>           */}
+          <Route path="/*" element={<Navigate to="/about" /> } />
+        </Routes>
+
+
     </>
   )
 }
