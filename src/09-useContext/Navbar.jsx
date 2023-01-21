70bd8da9d0ee9db6b5822ef48677b7b3c90415b8
Martin <martincraviotto@gmail.com>
2023-01-21 10:53:42 -0300

    Final Clase159

--- src/09-useContext/Navbar.jsx
+++ src/09-useContext/Navbar.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
-    <>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/login">Login</Link>
-    </>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded-3">
+        <div className="container-fluid">
+            <Link className="navbar-brand" to="/">useContext</Link>
+            <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+                
+                <NavLink 
+                    className={(props)=>`nav-link ${props.isActive ? 'active' : ''}` }
+                    to="/">
+                    Home
+                </NavLink>
+
+                <NavLink 
+                    // Otra forma de ver la propiedad isActive
+                    className={({isActive})=>`nav-link ${isActive ? 'active' : ''}` }
+                    to="/about">
+                    about
+                </NavLink>
+
+                <NavLink                     
+                    className={({isActive})=>`nav-link ${isActive ? 'active' : ''}` }
+                    to="/login">
+                    login
+                </NavLink>
+
+                                             
+            </ul>
+            </div>
+        </div>
+        </nav>
   )
 }
